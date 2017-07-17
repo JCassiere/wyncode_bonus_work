985f18ebc1d32adda1e9a69509e8777bd1d76cc0
Joe Cassiere <jcassiere91@gmail.com>
2017-07-17 16:15:32 -0400

    Added test to String Reverse

--- recursion/recursive_string_reverse.rb
+++ recursion/recursive_string_reverse.rb
@@ -1,11 +1,10 @@
 def recursive_string_reverse(string)
-
   if string.length == 1
     string
   else
     string[-1] + recursive_string_reverse(string.slice(0, string.length-1))
   end
-
 end
 
 puts recursive_string_reverse("yellow")
+puts recursive_string_reverse("The quick brown fox jumped over the lazy dog")
