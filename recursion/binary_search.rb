768a3546b9134c2d4b9f957ac11d3a6cc5429e7f
Joe Cassiere <jcassiere91@gmail.com>
2017-07-19 16:17:33 -0400

    Updated binary search file

--- recursion/binary_search.rb
+++ recursion/binary_search.rb
@@ -1,25 +1,22 @@
-def binary_search(array, num_to_find)
-  #returns the index of num_to_find in array
-  #or a string saying that num_to_find is not in the array
-  check_index = array.length/2 - 1
-  element = array[check_index]
-  if num_to_find == element
-    check_index
+def binary_search(array, num, start_pos=0)
+  #returns the index of num in array
+  #or -1 if num is not in array
+  mid_point = (array.length-1)/2
+  if num == array[mid_point]
+    start_pos + mid_point
   elsif array.length == 1
-    "The given number is not in the array"
-  elsif num_to_find < element
-    binary_search(array[0..check_index], num_to_find)
+    -1
+  elsif num < array[mid_point]
+    binary_search(array[0..mid_point], num, start_pos)
   else
-    begin
-      check_index + 1 + binary_search(array[(check_index + 1)..-1], num_to_find)
-    rescue TypeError
-      "The given number is not in the array"
-    end
+    binary_search(array[(mid_point + 1)..-1], num, start_pos + mid_point + 1)
   end
 end
 
-puts binary_search([1,3,5,6,8,33,345,567,678,789], 678)
-puts binary_search([1,3,5,6,8,33,345,567,678,789], 3)
-puts binary_search([1,3,5,6,8,33,345,567,678,789], 1)
-puts binary_search([1,3,5,6,8,33,345,567,678,789], 8)
-puts binary_search([1,3,5,6,8,33,345,567,678,789], 10)
+puts binary_search([1,3,5,6,8,33,345,567,678,789], 789) == 9
+puts binary_search([1,3,5,6,8,33,345,567,678,789], 678) == 8
+puts binary_search([1,3,5,6,8,33,345,567,678,789], 3) == 1
+puts binary_search([1,3,5,6,8,33,345,567,678,789], 1) == 0
+puts binary_search([1,3,5,6,8,33,345,567,678,789], 8) == 4
+puts binary_search([1,3,5,6,8,33,345,567,678,789], 10) == -1
+puts binary_search([1,3,5,6,8,33,345,567,678,789], 40) == -1
