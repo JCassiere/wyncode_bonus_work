7ae1cdcb7da26a04704c785a5b1a279b87ef8845
Joe Cassiere <jcassiere91@gmail.com>
2017-08-14 19:10:47 -0400

    reverted adjacency_list_hash to previous form

--- data_structures/ruby/graph/adjacency_list_hash/adjacency_list_hash.rb
+++ data_structures/ruby/graph/adjacency_list_hash/adjacency_list_hash.rb
@@ -4,25 +4,16 @@ class AdjacencyListHash
     #nodes will be an array of Node objects
     @nodes = {}
     node_list.each do |node|
-      @nodes[node] = Hash[node_list.map do |inner_node| 
-                       [inner_node, false]
-                     end
-                     ]
+      @nodes[node] = []
     end
   end
 
   def neighbors(node_name)
-    neighbors_array = []
-    @nodes[node_name].keys.each do |neighbor_node|
-      if @nodes[node_name][neighbor_node]
-        neighbors_array << neighbor_node
-      end
-    end
-    neighbors_array
+    @nodes[node_name]
   end
 
   def adjacent?(node_one, node_two)
-    @nodes[node_one][node_two]
+    neighbors(node_one).include?(node_two)
   end
 
   def nodes
@@ -31,13 +22,7 @@ class AdjacencyListHash
 
   def add_node(node_name)
     unless @nodes.keys.include?(node_name)
-      @nodes[node_name] = Hash[@nodes.map do |node| 
-                               [node, false]
-                          end
-                        ]
-      @nodes.keys.each do |node|
-        @nodes[node][node_name] = false
-      end
+      @nodes[node_name] = []
     end
   end
 
@@ -46,13 +31,15 @@ class AdjacencyListHash
   end
 
   def add_edge(node_one_name, node_two_name)
-    @nodes[node_one_name][node_two_name] = true
-    @nodes[node_two_name][node_one_name] = true
+    unless neighbors(node_one_name).include?(node_two_name)
+      @nodes[node_one_name] << node_two_name
+      @nodes[node_two_name] << node_one_name
+    end
   end
 
   def remove_edge(node_one_name, node_two_name)
-    @nodes[node_one_name][node_two_name] = false
-    @nodes[node_two_name][node_one_name] = false
+    @nodes[node_one_name].delete(node_two_name)
+    @nodes[node_two_name].delete(node_one_name)
   end
 
 end
