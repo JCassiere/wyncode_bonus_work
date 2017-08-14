f62c757c7c789761d52bd940e731859e50afbcf3
Joe Cassiere <jcassiere91@gmail.com>
2017-08-13 21:44:13 -0400

    added completed binary heap

---
+++ data_structures/ruby/heap/binary_heap.rb
@@ -0,0 +1,120 @@
+class MaxHeap
+	attr_reader :heap
+
+	def initialize(node_values=[])
+		@next_index = 0
+		@heap = []
+		unless node_values.empty?
+			@heap << node_values[0]
+			@next_index += 1
+			node_values[1..-1].each do |value|
+				insert(value)
+			end
+		end
+	end
+
+	def insert(value)
+		current_index = @next_index
+		@heap[current_index] = value
+		parent_index = get_parent_index(current_index)
+		parent_value = @heap[parent_index]
+		until value <= parent_value || current_index == 0
+			@heap[current_index] = parent_value
+			@heap[parent_index] = value
+			current_index = parent_index
+			parent_index = get_parent_index(current_index)
+			parent_value = @heap[parent_index]
+		end
+		@next_index += 1
+	end
+
+	def extract
+		prev_root = @heap[0]
+		@heap[0] = @heap.pop
+		current_index = 0
+		children = get_children(current_index)
+
+		until children[0].nil? || @heap[current_index] > children.max
+			if children[0] == children.max
+				switch_index = get_child_indices(current_index)[0]
+				switch_value = children[0]
+			else
+				switch_index = get_child_indices(current_index)[1]
+				switch_value = children[1]
+			end
+			@heap[switch_index] = @heap[current_index]
+			@heap[current_index] = switch_value
+			current_index = switch_index
+			children = get_children(current_index)
+		end
+		prev_root
+	end
+
+
+	private
+
+	def get_parent_index(child_index)
+		((child_index-1)/2).floor
+	end
+
+	def get_child_indices(parent_index)
+		[(2*parent_index+1), (2*parent_index+2)]
+	end
+
+	def get_children(parent_index)
+		children = get_child_indices(parent_index).map do |ind|
+			@heap[ind]
+		end
+		children
+	end
+end
+
+max_heap = MaxHeap.new([4, 11, 2, 8, 47, 9, 1, 1234, 10, 123])
+val = max_heap.extract
+p max_heap.heap
+
+
+class MinHeap < MaxHeap
+
+	def initialize(node_values = [])
+		super
+	end
+
+	def insert(value)
+		current_index = @next_index
+		@heap[current_index] = value
+		parent_index = get_parent_index(current_index)
+		parent_value = @heap[parent_index]
+		until value >= parent_value || current_index == 0
+			@heap[current_index] = parent_value
+			@heap[parent_index] = value
+			current_index = parent_index
+			parent_index = get_parent_index(current_index)
+			parent_value = @heap[parent_index]
+		end
+		@next_index += 1
+	end
+
+	def extract
+		prev_root = @heap[0]
+		@heap[0] = @heap.pop
+		current_index = 0
+		children = get_children(current_index)
+
+		until children[0].nil? || @heap[current_index] < children.min
+			if children[0] == children.min
+				switch_index = get_child_indices(current_index)[0]
+				switch_value = children[0]
+			else
+				switch_index = get_child_indices(current_index)[1]
+				switch_value = children[1]
+			end
+			@heap[switch_index] = @heap[current_index]
+			@heap[current_index] = switch_value
+			current_index = switch_index
+			children = get_children(current_index)
+		end
+		prev_root
+	end
+
+end
---
+++ data_structures/ruby/heap/spec/binary_heap_spec.rb
@@ -0,0 +1,76 @@
+require './binary_heap.rb'
+
+describe MaxHeap do
+
+	before(:each) do
+		@max_heap = MaxHeap.new([4, 2, 8, 9, 1, 10])
+	end
+	
+	context "creating a MaxHeap" do
+		it "creates an ordered binary heap" do
+			p @max_heap.heap
+			expect(@max_heap.heap).to eq([10, 8, 9, 2, 1, 4])
+		end
+	end
+
+	context "inserting a node" do
+		it "can insert a node" do
+			@max_heap.insert(10)
+			@max_heap.insert(7)
+			expect(@max_heap.heap).to eq([10, 8, 10, 7, 1, 4, 9, 2])
+		end
+	end
+
+	context "extracting the max value" do
+		before(:each) do
+			@max = @max_heap.extract
+		end
+
+		it "returns the max value" do
+			expect(@max).to eq(10)
+		end
+
+		it "reorders the heap properly" do
+			expect(@max_heap.heap).to eq([9, 8, 4, 2, 1])
+		end
+
+	end
+
+end
+
+describe MinHeap do
+
+	before(:each) do
+		@min_heap = MinHeap.new([4, 2, 8, 9, 1, 10])
+	end
+	
+	context "creating a MaxHeap" do
+		it "creates an ordered binary heap" do
+			expect(@min_heap.heap).to eq([1, 2, 8, 9, 4, 10])
+		end
+	end
+
+	context "inserting a node" do
+		it "can insert a node" do
+			@min_heap.insert(10)
+			@min_heap.insert(7)
+			expect(@min_heap.heap).to eq([1, 2, 8, 7, 4, 10, 10, 9])
+		end
+	end
+
+	context "extracting the min value" do
+		before(:each) do
+			@min = @min_heap.extract
+		end
+
+		it "returns the max value" do
+			expect(@min).to eq(1)
+		end
+
+		it "reorders the heap properly" do
+			expect(@min_heap.heap).to eq([2, 4, 8, 9, 10])
+		end
+
+	end
+
+end
